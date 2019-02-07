e46d1ab944912b359a15ce22fe908d88116e7a30
td94 <quynhtrangdo94@gmail.com>
2019-02-07 13:36:16 -0800

    Initial webpack setting

--- OverviewPhoto/webpack.config.js
+++ OverviewPhoto/webpack.config.js
@@ -1,23 +1,28 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client/src');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+const path = require('path');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+  mode: 'development',
+  entry: path.resolve(__dirname, 'client', 'src', 'index.js'),
   output: {
+    path: path.resolve(__dirname, 'client', 'dist'),
     filename: 'bundle.js',
-    path: DIST_DIR
   },
-  module : {
-    loaders : [
+  module: {
+    rules: [
       {
-        test : /\.jsx?/,
-        include : SRC_DIR,
-        loader : 'babel-loader',      
-        query: {
-          presets: ['react', 'es2015']
-        }
-      }
-    ]
-  }
+        test: /(.js$|.jsx$)/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devtool: 'source-map',
 };
