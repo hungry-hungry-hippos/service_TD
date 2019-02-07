c73afc47863676c2a080e53bd32e1dca1e71e2db
td94 <quynhtrangdo94@gmail.com>
2019-02-06 20:49:44 -0800

    Start on footer icons

--- Footer/src/App.jsx
+++ Footer/src/App.jsx
@@ -6,7 +6,30 @@ const { style } = require('./style.jsx');
 const App = () => (
   <div style={style.footertop}>
     <div style={style.row}>
-      FOOTER
+      <div className="col span-1-of-2">
+        <ul className="social-links">
+          <li>
+            <a href="#">
+              <ion-icon name="logo-facebook"></ion-icon>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <ion-icon name="logo-twitter"></ion-icon>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <ion-icon name="logo-googleplus"></ion-icon>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <ion-icon name="logo-instagram"></ion-icon>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 );
