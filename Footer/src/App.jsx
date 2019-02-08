7df842d3afb9ae7784edb13a9075f9701d12b46f
td94 <quynhtrangdo94@gmail.com>
2019-02-08 12:38:17 -0800

    Change in footer style

--- Footer/src/App.jsx
+++ Footer/src/App.jsx
@@ -5,30 +5,31 @@ const { style } = require('./style.jsx');
 
 const App = () => (
   <div style={style.footertop}>
-    <div style={style.row}>
-      <div className="col span-1-of-2">
-        <ul className="social-links">
-          <li>
-            <a href="#">
-              <ion-icon name="logo-facebook"></ion-icon>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <ion-icon name="logo-twitter"></ion-icon>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <ion-icon name="logo-googleplus"></ion-icon>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <ion-icon name="logo-instagram"></ion-icon>
-            </a>
-          </li>
-        </ul>
+    <div className="row no-gutters">
+      <div className="col col-12 col-lg-6 d-flex text-white">
+        <div className="flex-item">
+          <a href="#">
+            <ion-icon name="logo-facebook"></ion-icon>
+          </a>
+        </div>
+        <div className="flex-item">
+          <a href="#">
+            <ion-icon name="logo-twitter"></ion-icon>
+          </a>
+        </div>
+        <div className="flex-item">
+          <a href="#">
+            <ion-icon name="logo-googleplus"></ion-icon>
+          </a>
+        </div>
+        <div className="flex-item">
+          <a href="#">
+            <ion-icon name="logo-instagram"></ion-icon>
+          </a>
+        </div>
+      </div>
+      <div className="col col-12 col-lg-6 text-white">
+        Hi
       </div>
     </div>
   </div>
