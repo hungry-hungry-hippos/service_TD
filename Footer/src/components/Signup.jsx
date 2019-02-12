59641f6a9077df27a867298b64e64ef6eaf80388
td94 <quynhtrangdo94@gmail.com>
2019-02-11 16:34:13 -0800

    Add event handlers for signup component

--- Footer/src/components/Signup.jsx
+++ Footer/src/components/Signup.jsx
@@ -9,22 +9,39 @@ class Signup extends Component {
     };
   }
 
+  onChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  onClick() {
+    const { value } = this.state;
+    console.log(value);
+    // in realily, the email will be send to server
+  }
+
   render() {
     const { value } = this.state;
     return (
-      <div className="ml-5">
+      <div className="container mx-2">
         <h5>GET ZAGAT &apos; S NEWSLETTER IN YOUR INBOX...</h5>
         <div className="input-group">
           <input
             className="form-control"
-            type="text"
+            type="email"
             name="signup"
+            required
             placeholder="ENTER YOUR EMAIL"
             value={value}
-          // onChange={this.onChange.bind(this)}
+            onChange={this.onChange.bind(this)}
           />
           <div className="input-group-append">
-            <span className={`input-group-text ${style.button}`}>&#62;</span>
+            <button
+              type="button"
+              className={`input-group-text ${style.button}`}
+              onClick={this.onClick.bind(this)}
+            >
+              &#62;
+            </button>
           </div>
         </div>
       </div>
