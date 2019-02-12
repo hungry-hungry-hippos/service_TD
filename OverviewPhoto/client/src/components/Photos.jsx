4150b6abfb0723c9506159a65fc27a2f7cd93f7d
td94 <quynhtrangdo94@gmail.com>
2019-02-12 15:23:07 -0800

    Comment out Photos component

---
+++ OverviewPhoto/client/src/components/Photos.jsx
@@ -0,0 +1,15 @@
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import requestPhotos from ''
+
+// class Photos extends Component {
+//   render() {
+//     if (pending && !photos.length) {
+//       return (
+//         <div>Loading</div>
+//       )
+//     }
+//   }
+// }
+
+// export default connect(mapStateTo)
