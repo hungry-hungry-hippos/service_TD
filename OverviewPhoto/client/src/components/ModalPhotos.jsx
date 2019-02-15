912c12b81c4a34ab68331b081d968f8f0bec6dde
td94 <quynhtrangdo94@gmail.com>
2019-02-14 18:14:08 -0800

    Styling Modal

--- OverviewPhoto/client/src/components/ModalPhotos.jsx
+++ OverviewPhoto/client/src/components/ModalPhotos.jsx
@@ -23,10 +23,15 @@ class ModalPhotos extends Component {
     return (
       <div>
         {!showModal
-          && <button type="button" className={`${style.button} ModalPhotos`} onClick={this.toggleModal.bind(this)} data-toggle="modal" data-target="#ModalPhotos">{`${photos.length} PHOTOS ▷`}</button>}
+          && (
+            <button type="button" className={style.button} onClick={this.toggleModal.bind(this)}>
+              {`${photos.length} PHOTOS ▷`}
+            </button>
+          )
+        }
 
         <div
-          className="modal constainer"
+          className="modal"
           show={showModal.toString()}
           style={{
             display: `${showModal ? 'block' : 'none'}`,
@@ -38,19 +43,28 @@ class ModalPhotos extends Component {
           aria-labelledby="ModalPhotosCenterTitle"
           aria-hidden="true"
         >
-          <div className="modal-dialog" role="document">
-            <div className="modal-content bg-dark text-white">
-              <div className="modal-header">
-                <h5 className="modal-title">Restaurant Name</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true" className="text-white"> &times; </span>
+          <div className={`modal-dialog ${style.modalfull}`} role="document">
+            <div className={style.modalcontent}>
+              <div className="modal-header row">
+                {/* <div className="col col-9 text-center">
+                </div> */}
+                <h5 className="modal-title col-12 text-center">RESTAURANT NAME</h5>
+                <button type="button" className={`close ${style.closebutton}`} data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true" className="text-white col-1 mr-3"> &times; </span>
                 </button>
               </div>
-              <div className="modal-body">
+              <div className={`modal-body ${style.photodiv}`}>
                 {photos.length
-                  && <div className={style.photo}><img src={photos[0].photo_url} alt="" className={style.img} /></div>
+                  && (
+                    <div className={style.photos}>
+                      <div className={style.photo}><img src={photos[0].photo_url} alt="" className={style.img} /></div>
+                    </div>
+                  )
                 }
               </div>
+              {/* <div className="modal-footer">
+                end
+              </div> */}
             </div>
           </div>
         </div>
