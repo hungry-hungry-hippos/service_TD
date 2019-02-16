c493309ad1d4092f19fb362b82f8e923e5a62126
td94 <quynhtrangdo94@gmail.com>
2019-02-16 11:08:10 -0800

    Refactor CarouselModal to close on click

--- OverviewPhoto/client/dist/bundle.js
+++ OverviewPhoto/client/dist/bundle.js
@@ -277,50 +277,124 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
 /* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
-/* harmony import */ var _overview_module_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../overview.module.css */ "./client/src/overview.module.css");
-/* harmony import */ var _overview_module_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_overview_module_css__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! prop-types */ "./node_modules/prop-types/index.js");
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(prop_types__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _action_actions__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../action/actions */ "./client/src/action/actions.jsx");
+/* harmony import */ var _overview_module_css__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../overview.module.css */ "./client/src/overview.module.css");
+/* harmony import */ var _overview_module_css__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_overview_module_css__WEBPACK_IMPORTED_MODULE_4__);
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
 
 
 
-var CarouselModal = function CarouselModal(_ref) {
-  var openCarouselModal = _ref.openCarouselModal;
-  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, openCarouselModal && react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    className: _overview_module_css__WEBPACK_IMPORTED_MODULE_2___default.a.modal,
-    show: openCarouselModal.toString(),
-    style: {
-      display: 'block'
-    },
-    id: "ModalPhotos",
-    tabIndex: "-1",
-    role: "dialog" // onClick={this.toggleModal.bind(this)}
-    ,
-    "aria-labelledby": "ModalPhotosTitle",
-    "aria-hidden": "true"
-  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    role: "document"
-  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    className: _overview_module_css__WEBPACK_IMPORTED_MODULE_2___default.a.modalheader
-  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h5", null, "TESTING"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
-    type: "button",
-    className: "close ".concat(_overview_module_css__WEBPACK_IMPORTED_MODULE_2___default.a.closebutton),
-    "data-dismiss": "modal",
-    "aria-label": "Close"
-  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
-    "aria-hidden": "true",
-    className: "text-white"
-  }, " \xD7 "))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
-    className: _overview_module_css__WEBPACK_IMPORTED_MODULE_2___default.a.modalcontent
-  }, "HELLLLOOOOOOOO"))));
-};
+
+
+
+var CarouselModal =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(CarouselModal, _Component);
+
+  function CarouselModal() {
+    _classCallCheck(this, CarouselModal);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(CarouselModal).apply(this, arguments));
+  }
+
+  _createClass(CarouselModal, [{
+    key: "toggleCarouselModal",
+    value: function toggleCarouselModal() {
+      var onModalRequest = this.props.onModalRequest;
+      onModalRequest();
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this$props = this.props,
+          openCarouselModal = _this$props.openCarouselModal,
+          photos = _this$props.photos,
+          name = _this$props.name;
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, openCarouselModal && react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: _overview_module_css__WEBPACK_IMPORTED_MODULE_4___default.a.modal,
+        show: openCarouselModal.toString(),
+        style: {
+          display: 'block'
+        },
+        id: "ModalPhotos",
+        tabIndex: "-1",
+        role: "dialog",
+        "aria-labelledby": "ModalPhotosTitle",
+        "aria-hidden": "true"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        role: "document"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: _overview_module_css__WEBPACK_IMPORTED_MODULE_4___default.a.modalheader
+      }, name && react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h5", {
+        className: "modal-title text-center"
+      }, name.toUpperCase()), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+        type: "button",
+        className: "close ".concat(_overview_module_css__WEBPACK_IMPORTED_MODULE_4___default.a.closebutton),
+        "data-dismiss": "modal",
+        "aria-label": "Close",
+        onClick: this.toggleCarouselModal.bind(this)
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+        "aria-hidden": "true",
+        className: "text-white"
+      }, " \xD7 "))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: _overview_module_css__WEBPACK_IMPORTED_MODULE_4___default.a.modalcontent
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+        src: photos[0].photo_url,
+        alt: "",
+        style: {
+          height: '50%',
+          width: '70%'
+        }
+      })))));
+    }
+  }]);
+
+  return CarouselModal;
+}(react__WEBPACK_IMPORTED_MODULE_0__["Component"]);
 
 var mapStateToProps = function mapStateToProps(state) {
   return {
+    name: state.photos.name,
+    photos: state.photos.photos,
     openCarouselModal: state.photos.openCarouselModal
   };
 };
 
-/* harmony default export */ __webpack_exports__["default"] = (Object(react_redux__WEBPACK_IMPORTED_MODULE_1__["connect"])(mapStateToProps)(CarouselModal));
+var mapDispatchToProps = function mapDispatchToProps(dispatch) {
+  return {
+    onModalRequest: function onModalRequest() {
+      return dispatch(Object(_action_actions__WEBPACK_IMPORTED_MODULE_3__["displayCarouselModal"])());
+    }
+  };
+};
+
+CarouselModal.propTypes = {
+  name: prop_types__WEBPACK_IMPORTED_MODULE_2___default.a.string.isRequired,
+  photos: prop_types__WEBPACK_IMPORTED_MODULE_2___default.a.instanceOf(Array).isRequired,
+  openCarouselModal: prop_types__WEBPACK_IMPORTED_MODULE_2___default.a.bool.isRequired,
+  onModalRequest: prop_types__WEBPACK_IMPORTED_MODULE_2___default.a.func.isRequired
+};
+/* harmony default export */ __webpack_exports__["default"] = (Object(react_redux__WEBPACK_IMPORTED_MODULE_1__["connect"])(mapStateToProps, mapDispatchToProps)(CarouselModal));
 
 /***/ }),
 
--- OverviewPhoto/client/src/components/CarouselModal.jsx
+++ OverviewPhoto/client/src/components/CarouselModal.jsx
@@ -1,46 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Proptypes from 'prop-types';
+import { displayCarouselModal } from '../action/actions';
 import style from '../overview.module.css';
 
-const CarouselModal = ({openCarouselModal}) => (
-  <div>
-    {openCarouselModal
-      && (
-        <div
-          className={style.modal}
-          show={openCarouselModal.toString()}
-          style={{
-            display: 'block',
-          }}
-          id="ModalPhotos"
-          tabIndex="-1"
-          role="dialog"
-          // onClick={this.toggleModal.bind(this)}
-          aria-labelledby="ModalPhotosTitle"
-          aria-hidden="true"
-        >
-          <div role="document">
-            <div className={style.modalheader}>
-              {/* {name && <h5 className="modal-title text-center">{name.toUpperCase()}</h5>} */}
-              <h5>TESTING</h5>
-              <button type="button" className={`close ${style.closebutton}`} data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true" className="text-white"> &times; </span>
-              </button>
+class CarouselModal extends Component {
+  toggleCarouselModal() {
+    const { onModalRequest } = this.props;
+    onModalRequest();
+  }
+
+  render() {
+    const { openCarouselModal, photos, name } = this.props;
+    return (
+      <div>
+        {openCarouselModal
+          && (
+            <div
+              className={style.modal}
+              show={openCarouselModal.toString()}
+              style={{
+                display: 'block',
+              }}
+              id="ModalPhotos"
+              tabIndex="-1"
+              role="dialog"
+              aria-labelledby="ModalPhotosTitle"
+              aria-hidden="true"
+            >
+              <div role="document">
+                <div className={style.modalheader}>
+                  {name && <h5 className="modal-title text-center">{name.toUpperCase()}</h5>}
+                  <button
+                    type="button"
+                    className={`close ${style.closebutton}`}
+                    data-dismiss="modal"
+                    aria-label="Close"
+                    onClick={this.toggleCarouselModal.bind(this)}
+                  >
+                    <span aria-hidden="true" className="text-white"> &times; </span>
+                  </button>
+                </div>
+                <div className={style.modalcontent}>
+                  <img src={photos[0].photo_url} alt="" style={{ height: '50%', width: '70%' }} />
+                </div>
+              </div>
             </div>
-            <div className={style.modalcontent}>
-              HELLLLOOOOOOOO
-            </div>
-          </div>
-        </div>
-      )
-    }
-  </div>
-)
+          )
+        }
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => (
   {
+    name: state.photos.name,
+    photos: state.photos.photos,
     openCarouselModal: state.photos.openCarouselModal,
   }
-)
+);
 
-export default connect(mapStateToProps)(CarouselModal);
+const mapDispatchToProps = dispatch => (
+  {
+    onModalRequest: () => dispatch(displayCarouselModal()),
+  }
+);
+
+CarouselModal.propTypes = {
+  name: Proptypes.string.isRequired,
+  photos: Proptypes.instanceOf(Array).isRequired,
+  openCarouselModal: Proptypes.bool.isRequired,
+  onModalRequest: Proptypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CarouselModal);
